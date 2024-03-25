37f180a309d005d1b8c2cc8dc49cb288ddb1d926
warspoon <azerty12345@mail.ru>
2024-03-25 12:23:51 +0000

    changes

--- terraform/network.tf
+++ terraform/network.tf
@@ -9,40 +9,45 @@ resource "vkcs_networking_network" "network" {
 resource "vkcs_networking_subnet" "subnetwork" {
    name       = "subnet_1"
    network_id = vkcs_networking_network.network.id
-   cidr       = "192.168.199.0/24"
+   cidr       = "192.168.199.0/24"   
 }
 
-#resource "vkcs_networking_router" "router" {
-#   name                = "router"
-#   admin_state_up      = true
-#   external_network_id = data.vkcs_networking_network.extnet.id
-#}
+resource "vkcs_networking_router" "router" {
+   name                = "router"
+   admin_state_up      = true
+   external_network_id = data.vkcs_networking_network.extnet.id
+}
 
-#resource "vkcs_networking_router_interface" "db" {
-#   router_id = vkcs_networking_router.router.id
-#   subnet_id = vkcs_networking_subnet.subnetwork.id
-#}
+resource "vkcs_networking_router_interface" "db" {
+   router_id = vkcs_networking_router.router.id
+   subnet_id = vkcs_networking_subnet.subnetwork.id
+}
 
-#resource "vkcs_networking_secgroup_rule" "secgroup_rule_1" {
-#   direction = "ingress"
-#   ethertype = "IPv4"
-#   port_range_max = 22
-#   port_range_min = 22
-#   protocol = "tcp"
-#   remote_ip_prefix = "0.0.0.0/0"
-#   security_group_id = vkcs_networking_secgroup.secgroup.id
-#   description = "secgroup_rule_1"
-#}
+resource "vkcs_networking_secgroup" "secgroup" {
+   name = "security_group"
+   description = "terraform security group"
+}
 
-#resource "vkcs_networking_secgroup_rule" "secgroup_rule_2" {
-#   direction = "ingress"
-#   ethertype = "IPv4"
-#   port_range_max = 80
-#   port_range_min = 80
-#   remote_ip_prefix = "0.0.0.0/0"
-#   protocol = "tcp"
-#   security_group_id = vkcs_networking_secgroup.secgroup.id
-#}
+resource "vkcs_networking_secgroup_rule" "secgroup_rule_1" {
+   direction = "ingress"
+   ethertype = "IPv4"
+   port_range_max = 22
+   port_range_min = 22
+   protocol = "tcp"
+  remote_ip_prefix = "0.0.0.0/0"
+   security_group_id = vkcs_networking_secgroup.secgroup.id
+   description = "secgroup_rule_1"
+}
+
+resource "vkcs_networking_secgroup_rule" "secgroup_rule_2" {
+   direction = "ingress"
+   ethertype = "IPv4"
+   port_range_max = 80
+   port_range_min = 80
+   remote_ip_prefix = "0.0.0.0/0"
+   protocol = "tcp"
+   security_group_id = vkcs_networking_secgroup.secgroup.id
+}
 
 #resource "vkcs_networking_port" "port" {
 #   name = "port_1"
